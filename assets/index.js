256a0cc1b4bd9240aaa5b1c42c9f3cfffab5338e
wojtuus16 <165338767+wojtuus16@users.noreply.github.com>
2025-11-05 18:36:30 +0100

    index.js

--- assets/index.js
+++ assets/index.js
@@ -90,10 +90,11 @@ function showErrorState() {
 }
 
 function forwardToId(params) {
-  location.href = `/FistaszjoObywatel/id?${params}`;
+  location.href = ./id.html?${params};
 }
 
 const guide = document.querySelector('.guide_holder');
 guide.addEventListener('click', () => {
   guide.classList.toggle('unfolded');
 });
+
